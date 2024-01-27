56d39681bb1f1dcfd3882e0035c43b5c88e4410c
a.code <huangyp1991@gmail.com>
2024-01-27 23:30:52 +0800

    js:vue:single-file-component

---
+++ vue/sfc/App.vue
@@ -0,0 +1,12 @@
+<template>
+<div>
+    <TodoList></TodoList>
+</div>
+</template>
+
+<script>
+import TodoList from './TodoList.vue';
+export default {
+    components: {TodoList}
+};
+</script>
---
+++ vue/sfc/TodoList.vue
@@ -0,0 +1,14 @@
+<template>
+<ul>
+    <!-- <li v-for="item in itemList" v-bind:key="item.id">{{item.name}}</li> -->
+    <li>test</li>
+</ul>
+</template>
+
+<script>
+export default {
+    data() {
+        msg: 'hi'
+    }
+}
+</script>
